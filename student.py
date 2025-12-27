309d1d6301fb4962e403d185d7dafdb9fb2cabaf
vekasni3172005-lgtm <vekasni3172005@gmail.com>
2025-12-28 02:44:15 +0530

    min

---
+++ student.py
@@ -0,0 +1,8 @@
+
+
+def register_student(name, roll_no):
+    print(f"Student Registered: {name}, Roll No: {roll_no}")
+
+def display_student(name, roll_no):
+    print(f"Student Name: {name}")
+    print(f"Roll Number: {roll_no}")
